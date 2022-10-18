5989068c01f3e64f9c901b6842320cdd1703a592
raghavt20 <raghavt20@aospa.co>
2022-10-18 17:44:03 +0530

    xcalibur: Rewrite script

--- build.sh
+++ build.sh
@@ -8,20 +8,21 @@ builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image.gz-dtb
 kernel_name="xcalibur-violet"
 zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
-TC_DIR=$HOME/tc/proton-clang
+TC_DIR=$HOME/tc/
+CLANG_DIR=$TC_DIR/clang-r458507
 export CONFIG_FILE="vendor/violet-perf_defconfig"
 export ARCH="arm64"
 export KBUILD_BUILD_HOST=SuperiorOS
 export KBUILD_BUILD_USER=Joker-V2
 
-export PATH="$TC_DIR/bin:$PATH"
+export PATH="$CLANG_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-echo "Proton Clang not found! Cloning to $TC_DIR..."
-if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
+    echo "Toolchain not found! Cloning to $TC_DIR..."
+    if ! git clone -q --depth=1 --single-branch https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master $TC_DIR; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
 fi
 
 # Colors
@@ -32,38 +33,35 @@ LGR='\033[1;32m'
 
 make_defconfig()
 {
-	# Needed to make sure we get dtb built and added to kernel image properly
-     START=$(date +"%s")
-	echo -e ${LGR} "############### Cleaning ################${NC}"
-    rm $anykernel/Image.gz-dtb
-    rm -rf $ZIMAGE
-
-	echo -e ${LGR} "########### Generating Defconfig ############${NC}"
+    START=$(date +"%s")
+    echo -e ${LGR} "########### Generating Defconfig ############${NC}"
     make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc --all)
 }
 compile()
 {
-	cd ${kernel_dir}
-	echo -e ${LGR} "######### Compiling kernel #########${NC}"
-	make -j$(nproc --all) O=out \
-                      ARCH=${ARCH}\
-                      CC="ccache clang" \
-	                CLANG_TRIPLE="aarch64-linux-gnu-" \
-	                CROSS_COMPILE="aarch64-linux-gnu-" \
-	                CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
-                        -j$(nproc --all)
+    cd ${kernel_dir}
+    echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=${ARCH}\
+    CC="ccache clang" \
+    CLANG_TRIPLE="aarch64-linux-gnu-" \
+    CROSS_COMPILE="aarch64-linux-gnu-" \
+    CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+    LLVM=1 \
+    LLVM_IAS=1
 }
 
-completion() 
+completion()
 {
-	cd ${objdir}
-	COMPILED_IMAGE=arch/arm64/boot/Image.gz-dtb
-	COMPILED_DTBO=arch/arm64/boot/dtbo.img
-	if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+    cd ${objdir}
+    COMPILED_IMAGE=arch/arm64/boot/Image.gz-dtb
+    COMPILED_DTBO=arch/arm64/boot/dtbo.img
+    if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
 
-	git clone -q https://github.com/Joker-V2/AnyKernel3 $anykernel
+        git clone -q https://github.com/Joker-V2/AnyKernel3 $anykernel
 
-		mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
+        mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
 
         cd $anykernel
         find . -name "*.zip" -type f
@@ -71,18 +69,21 @@ completion()
         zip -r AnyKernel.zip *
         mv AnyKernel.zip $zip_name
         mv $anykernel/$zip_name $HOME/$zip_name
-	rm -rf $anykernel
+        rm -rf $anykernel
         END=$(date +"%s")
         DIFF=$(($END - $START))
         curl --upload-file $HOME/$zip_name https://free.keep.sh; echo
-		echo -e ${LGR} "############################################"
-		echo -e ${LGR} "############# OkThisIsEpic!  ##############"
-		echo -e ${LGR} "############################################${NC}"
-	else
-		echo -e ${RED} "############################################"
-		echo -e ${RED} "##         This Is Not Epic :'(           ##"
-		echo -e ${RED} "############################################${NC}"
-	fi
+        rm $HOME/$zip_name
+        echo -e ${LGR} "############################################"
+        echo -e ${LGR} "############# OkThisIsEpic!  ##############"
+        echo -e ${LGR} "############################################${NC}"
+        exit 0
+    else
+        echo -e ${RED} "############################################"
+        echo -e ${RED} "##         This Is Not Epic :'(           ##"
+        echo -e ${RED} "############################################${NC}"
+        exit 1
+    fi
 }
 make_defconfig
 compile
